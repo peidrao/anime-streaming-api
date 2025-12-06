964190e69fc46b8f13453df1dff56abcf9d0d792
Pedro Fonseca <contatopedrorn@gmail.com>
2025-12-06 09:34:21 -0300

    refactor: 🗑️ remove unused anime app files

--- anime/choices.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class AnimeStatus(models.TextChoices):
-    ANNOUNCED = 'announced', 'Announced'
-    HIATUS = 'hiatus', 'Hiatus'
-    FINISHED = 'finished', 'Finished'
